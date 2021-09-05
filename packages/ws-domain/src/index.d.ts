48d9fe14ff8a164e832faea727c2581aae546db5
Ashlin <ashlin.inwood@gmail.com>
2021-09-05 15:50:07 +1200

    Clean up and setting changes

--- packages/ws-domain/src/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { DomainConnection } from "./src/DomainConnection";
-export { WebSocketDomainServer } from "./src/WebSocketDomainServer";
--- packages/ws-domain/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WebSocketDomainServer = exports.DomainConnection = void 0;
-var DomainConnection_1 = require("./src/DomainConnection");
-Object.defineProperty(exports, "DomainConnection", { enumerable: true, get: function () { return DomainConnection_1.DomainConnection; } });
-var WebSocketDomainServer_1 = require("./src/WebSocketDomainServer");
-Object.defineProperty(exports, "WebSocketDomainServer", { enumerable: true, get: function () { return WebSocketDomainServer_1.WebSocketDomainServer; } });
