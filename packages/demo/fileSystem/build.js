d523edf6cbb7a8ad570a3b5eff5f2fb507dcec91
Ashlin <ashlin.inwood@gmail.com>
2021-09-29 18:22:13 +1300

    Fix build script

--- packages/demo/fileSystem/build.js
+++ packages/demo/fileSystem/build.js
@@ -1,19 +1,20 @@
 let browserify = require('browserify');
 let babelify = require("babelify");
 let fs = require("fs");
+let path = require('path');
 
 
 try {
-    fs.mkdirSync("./dest");
+    fs.mkdirSync(path.join(__dirname, "dest"));
 } catch (e) {}
-fs.copyFileSync("./src/index.html", "./dest/index.html");
+fs.copyFileSync(path.join(__dirname, "src/index.html"), path.join(__dirname, "dest/index.html"));
 
 
-browserify("./src/index.js")
+browserify(path.join(__dirname, "src/index.js"))
     .transform(babelify, {
         global: true,
         plugins: ['@babel/plugin-transform-modules-commonjs', 'babel-plugin-transform-dynamic-imports-to-static-imports'],
         ignore: [/\/node_modules\/(?!browser-fs-access\/)/]
     })
     .bundle()
-    .pipe(fs.createWriteStream('./dest/bundle.js'));
+    .pipe(fs.createWriteStream(path.join(__dirname, 'dest/bundle.js')));
--- packages/demo/rtc connection/build.js
+++ packages/demo/rtc connection/build.js
@@ -1,9 +1,10 @@
 let browserify = require('browserify');
 let fs = require("fs");
+let path = require('path');
 
 try {
-    fs.mkdirSync("./dest");
+    fs.mkdirSync(path.join(__dirname, "dest"));
 } catch (e) {}
-fs.copyFileSync("./src/index.html", "./dest/index.html");
+fs.copyFileSync(path.join(__dirname, "src/index.html") , path.join(__dirname, "dest/index.html"));
 
-browserify("./src/index.js").bundle().pipe(fs.createWriteStream('./dest/bundle.js'));
+browserify(path.join(__dirname, "src/index.js")).bundle().pipe(fs.createWriteStream(path.join(__dirname, "dest/bundle.js")));
