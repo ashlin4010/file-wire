0a127f4ebbf6c32b390b60674622c0874b4333d7
Ashlin <ashlin.inwood@gmail.com>
2021-10-19 20:30:39 +1300

    Added cross platform environment variables and pkg build task

---
+++ packages/client-cli/config.ts
@@ -0,0 +1,18 @@
+import * as fs from "fs";
+import * as path from "path";
+
+const config_path = path.join(process.cwd(), "config.json");
+
+let config: any = {
+    domain: (Math.random() + 1).toString(36).substring(7),
+    path: "./share"
+};
+
+try {
+    config = JSON.parse(fs.readFileSync(config_path, {encoding: "utf8"}));
+} catch(error: any) {
+    if (error.code !== 'ENOENT') throw error;
+    fs.writeFileSync(config_path, JSON.stringify(config,null,2));
+}
+
+module.exports = config;
--- packages/client-cli/index.ts
+++ packages/client-cli/index.ts
@@ -2,23 +2,63 @@ import { DomainConnection } from "ws-domain";
 import { FileSystemInterface } from "common-file-system";
 import { RTCConnection, WsDuplex } from "rtc-connection";
 import { RTCController } from "rtc-controller";
+import * as config from "./config";
 
-const isProduction = process.env.NODE_ENV === "production";
-const serverAdr = process.env.SERVER_ADR;
+const isProduction = process.env.NODE_ENV !== "development";
+const serverAdr = process.env.SERVER_ADR || "filewire.io";
 let url = isProduction ? `wss://${serverAdr}` : "ws://127.0.0.1:8080";
 
-let path = "./testdata"
+console.log(config);
+
+// @ts-ignore
+let path = config["path"];
+// @ts-ignore
+let domain = config["domain"];
 let isServer = true;
 let isInitiator = !isServer;
-let domain = "testing";
-let domainConnection;
+let domainConnection: DomainConnection;
 
 // start client
 log("Domain:", domain);
 log("URL:", url);
 log("Connecting to signalling server...");
 
-tryConnect().then(async (wsProxy) => {
+
+tryConnect().then(connect).catch(connectFailed);
+
+
+function log(...data: any) {console.log("[Host]:", ...data);}
+
+function tryConnect(maxRetries: number = 5): Promise<any>  {
+    return new Promise((resolve, reject) => {
+        let attempt = 0;
+        let retry = setInterval(() => {
+
+            domainConnection = new DomainConnection(url, domain, isServer ? "token" : undefined);
+            domainConnection.on("error", (err) => {
+                attempt += 1;
+                log("Websocket connection failed to be established, the target might not be online, retrying in 1 second");
+                if(attempt > maxRetries) reject("Websocket connection failed to be established");
+            });
+
+            domainConnection.on("connect", async (wsProxy) => {
+                clearInterval(retry);
+                resolve({domainConnection, wsProxy});
+            });
+
+            // domainConnection.on("disconnect", ((code, reason) => {
+            //     console.log("disconnect from domain,", code, reason);
+            // }));
+        }, 1000);
+    });
+}
+
+function connectFailed() {
+    log("Failed to connect to signalling server, stopping");
+    process.exit(1);
+}
+
+async function connect({domainConnection, wsProxy}: any) {
     log("Connected to signalling server");
     if(isServer) {
         wsProxy.on("connection", (ws: any) => {
@@ -50,37 +90,11 @@ tryConnect().then(async (wsProxy) => {
         });
     }
 
-}).catch(() => {
-    log("Failed to connect to signalling server, stopping");
-    process.exit(0);
-});
-
-
-function log(...data: any) {
-    console.log("[Host]:", ...data);
+    domainConnection.on("disconnect", ((code:number, reason: string) => {
+        log("Connection to signalling server closed code:", code, "reason:", reason);
+        domainConnection.removeAllListeners();
+        tryConnect().then(connect).catch(connectFailed);
+    }));
 }
 
-function tryConnect(maxRetries: number = 5): Promise<any>  {
-    return new Promise((resolve, reject) => {
-        let attempt = 0;
-        let retry = setInterval(() => {
 
-            domainConnection = new DomainConnection(url, domain, isServer ? "token" : undefined);
-            domainConnection.on("error", (err) => {
-                attempt += 1;
-                log("Websocket connection failed to be established, the target might not be online, retrying in 1 second");
-                if(attempt > maxRetries) reject("Websocket connection failed to be established");
-            });
-
-            domainConnection.on("connect", async (wsProxy) => {
-                clearInterval(retry);
-                resolve(wsProxy);
-            });
-
-            domainConnection.on("disconnect", ((code, reason) => {
-                console.log("disconnect from domain,", code, reason);
-            }));
-
-        }, 1000);
-    });
-}
